1d4cb9aa81dde11e32568afb799de6db8dac7a76
David Swift <david.swift@valimail.com>
2018-01-25 12:07:57 -0800

    Line up all the build directories.

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,14 +2,15 @@
 REV=$(git rev-parse HEAD | cut -c1-6)
 DEST="${PKG_ROOT}/darwin_amd64"
 
-mkdir -p "${DEST}"
-
 BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%S%z)
 BUILD_FLAGS="-X main.Version=${REV} -X main.BuildDate='${BUILD_DATE}'"
 
 #
-# We follow the naming convention at https://www.terraform.io/docs/configuration/providers.html,
-# appending a '_valimail' string in github as part of the version tag for clarity
+# We follow the naming convention at https://www.terraform.io/docs/configuration/providers.html#third-party-plugins,
+# appending a '_valimail.#' string in github as part of the version tag for clarity
 #
-GOOS=darwin GOARCH=amd64 go build -o "${DEST}/terraform-provider-aws_${CIRCLE_TAG}" main.go
+cd "${GOPATH}/${SRC_ROOT}"
+mkdir -p "${DEST}"
+
+GOOS=darwin GOARCH=amd64 go build -o "${DEST}/terraform-provider-aws_${CIRCLE_TAG}" terraform-provider-aws/main.go
 
