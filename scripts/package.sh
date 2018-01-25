d32be0ec9dac58ab03ffb777919e9f865133ed38
David Swift <david.swift@valimail.com>
2018-01-25 11:30:32 -0800

    Remove unneeded directory change. It is already in the correct location when the script is run.

--- scripts/package.sh
+++ scripts/package.sh
@@ -11,6 +11,5 @@ BUILD_FLAGS="-X main.Version=${REV} -X main.BuildDate='${BUILD_DATE}'"
 # We follow the naming convention at https://www.terraform.io/docs/configuration/providers.html,
 # appending a '_valimail' string in github as part of the version tag for clarity
 #
-cd "${GOPATH}/${SRC_ROOT}"
-GOOS=darwin GOARCH=amd64 go build -o "${DEST}/terraform-provider-aws_${CIRCLE_TAG}" terraform-provider-aws/main.go
+GOOS=darwin GOARCH=amd64 go build -o "${DEST}/terraform-provider-aws_${CIRCLE_TAG}" main.go
 
